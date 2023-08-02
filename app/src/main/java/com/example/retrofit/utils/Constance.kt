0067f5c23b18e1b86124678b745d2f7fed9bc581
Parham <parhamchary@gmail.com>
2023-08-02 20:46:04 +0430

    Initial commit

--- app/src/main/java/com/example/retrofit/utils/Constance.kt
+++ app/src/main/java/com/example/retrofit/utils/Constance.kt
@@ -2,5 +2,5 @@ package com.example.retrofit.utils
 
 object Constance {
     const val BASE_URL = "https://jsonplaceholder.typicode.com"
-    const val END_POINT = "/posts"
+    const val END_POINT = "/posts/1"
 }
